0cf3d3cef8eb5ffa8e14f487914d6780a9dca57e
unknown <rubikonspace@ukr.net>
2023-10-25 00:34:19 +0300

    cast

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,18 +1,53 @@
+import { getMovieCredits } from 'helpers/API';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 export default function Cast() {
+  const [aboutCasts, setAboutCasts] = useState([]);
+  const params = useParams();
+
+  // console.log(params.movieId);
+  // console.log(aboutCasts);
+
+  useEffect(() => {
+    // if (aboutCasts.length === 0) return;
+
+    const fetchMovieCredits = async () => {
+      try {
+        const infoCast = await getMovieCredits(params.movieId);
+
+        console.log(infoCast);
+
+        setAboutCasts(infoCast.cast);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (params.movieId) {
+      fetchMovieCredits();
+    }
+    // fetchMovieCredits();
+  }, [params.movieId]);
+
   return (
-    <>
-      {/* <div className="actor-card">
-        <img
-          src={`https://image.tmdb.org/t/p/w200${profile_path}`}
-          alt={name}
-          className="actor-image"
-        />
-        <div className="actor-details">
-          <h3 className="actor-name">{name}</h3>
-          <p className="actor-character">Character: {character}</p>
-          <p className="actor-popularity">Popularity: {popularity}</p>
-        </div>
-      </div> */}
-    </>
+    <div className="actor-card">
+      {aboutCasts.length !== 0 &&
+        aboutCasts.map(({ id, profile_path, name, character }) => {
+          return (
+            <div key={id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w200${profile_path}`}
+                alt={name}
+                className="actor-image"
+              />
+              <div className="actor-details">
+                <h3 className="actor-name">{name}</h3>
+                <p className="actor-character">Character: {character}</p>
+              </div>
+            </div>
+          );
+        })}
+    </div>
   );
 }
