c7ba282542e091367806de6785638996021f38e0
unknown <rubikonspace@ukr.net>
2023-10-24 23:07:04 +0300

    у

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,4 +1,28 @@
 export default function Reviews({ movieId }) {
   console.log(movieId);
-  return <div>Reviews</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime totam
+      quod, sit eos repellat dolore ex rerum dolorum alias animi fugiat
+      quibusdam repudiandae placeat praesentium fuga labore numquam error
+      ratione cum ipsum vel consequatur iure deleniti? Magnam inventore totam,
+      molestias vitae distinctio repellat earum eligendi officiis error aperiam
+      voluptas? Aliquid dolor fugiat et quidem eos sunt impedit earum qui fugit
+      vel obcaecati iusto, fuga nemo corrupti incidunt? Sapiente, recusandae at!
+      Quaerat error delectus nobis, alias in dolor labore iusto dicta ea
+      molestiae. Ratione nam dolores vero, voluptatum, minus recusandae deleniti
+      temporibus ea sequi alias quis fuga exercitationem? Unde ullam assumenda
+      amet, magnam minima eum inventore quas id veniam. Quae quidem sapiente
+      nulla ducimus atque vitae enim voluptas repellendus? Maiores facere
+      consequatur beatae at eligendi error libero, quia est nesciunt praesentium
+      odit laborum tempore corporis aspernatur. Totam sint itaque, commodi
+      voluptate omnis expedita aspernatur. Possimus quasi asperiores animi nam
+      dolore minus tempore et repudiandae, incidunt pariatur mollitia delectus,
+      molestias dolores odit molestiae vitae aliquam magni! Aliquid deleniti
+      voluptatem iste quo, non saepe vitae blanditiis, perferendis asperiores
+      velit recusandae ullam qui a corporis repellendus facilis amet! Maiores ad
+      deserunt commodi nobis, totam quam perspiciatis nisi facilis obcaecati
+      eum, molestias vitae non harum!
+    </div>
+  );
 }
