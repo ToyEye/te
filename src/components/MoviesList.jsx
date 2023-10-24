57f640327ce9c7ed9ad8260eafbefcfb2249687f
unknown <rubikonspace@ukr.net>
2023-10-24 21:26:12 +0300

    3

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -3,21 +3,23 @@ import { Link } from 'react-router-dom';
 export default function MovieList({ trendMovies }) {
   return (
     <ul>
-      {trendMovies.map(
-        ({ id, title, overview, original_title, original_name, name }) => {
-          const displayTitle = title || original_title || name || original_name;
-          return (
-            <Link key={id} to={`/Movie/${id}`}>
-              <li
-                key={id}
-                style={{ marginLeft: '40px', listStyleType: 'disc' }}
-              >
-                {displayTitle}
-              </li>
-            </Link>
-          );
-        }
-      )}
+      {trendMovies &&
+        trendMovies.map(
+          ({ id, title, overview, original_title, original_name, name }) => {
+            const displayTitle =
+              title || original_title || name || original_name;
+            return (
+              <Link key={id} to={`/Movie/${id}`}>
+                <li
+                  key={id}
+                  style={{ marginLeft: '40px', listStyleType: 'disc' }}
+                >
+                  {displayTitle}
+                </li>
+              </Link>
+            );
+          }
+        )}
     </ul>
   );
 }
--- src/components/pages/MovieDetails.jsx
+++ src/components/pages/MovieDetails.jsx
@@ -2,20 +2,18 @@ import { getMovieDetails } from 'helpers/API';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-const MovieDetails = () => {
+export default function MovieDetails() {
   const [aboutMovie, setAboutMovie] = useState(null);
-  const { movieId } = useParams();
-  console.log(movieId);
-  // console.log(aboutMovie);
+  const params = useParams();
 
   useEffect(() => {
-    if (!movieId) {
+    if (!params.movieId) {
       return;
     }
 
     const fetchMovieDetails = async () => {
       try {
-        const detailInfo = await getMovieDetails(movieId);
+        const detailInfo = await getMovieDetails(params.movieId);
         setAboutMovie(detailInfo);
       } catch (err) {
         console.log(err);
@@ -23,26 +21,25 @@ const MovieDetails = () => {
     };
 
     fetchMovieDetails();
-  }, [movieId]);
+  }, [params.movieId]);
 
   return (
     <div>
-      {/* <h1>{aboutMovie.original_title}</h1> */}
-      <p>User Score: 75%</p>
-      <h2>Overview</h2>
-      {/* <p>{aboutMovie.overview}</p> */}
-      <h2>Genres</h2>
-      <ul>
-        {/* {aboutMovie.genres.map(genre => {
-          return (
-            <>
-              <li key={genre.id}>{genre.name}</li>
-            </>
-          );
-        })} */}
-      </ul>
+      {aboutMovie && (
+        <>
+          <h1>{aboutMovie.original_title}</h1>
+          <p>User Score: {Math.ceil(aboutMovie.vote_average * 10)}%</p>
+          <h2>Overview</h2>
+          <p>{aboutMovie ? aboutMovie.overview : 'No overview available'}</p>
+          <h2>Genres</h2>
+          <ul>
+            {aboutMovie &&
+              aboutMovie.genres.map(genre => {
+                return <li key={genre.id}>{genre.name}</li>;
+              })}
+          </ul>
+        </>
+      )}
     </div>
   );
-};
-
-export default MovieDetails;
+}
