7279f5a22c0c452e295b55f5a3b65b699df9549b
unknown <rubikonspace@ukr.net>
2023-10-25 01:13:12 +0300

    3

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* basename="/KRB-U" */}
     {/* basename="/goit-react-hw-05-movies" */}
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <App />
       <GlobalStyle />
     </BrowserRouter>
