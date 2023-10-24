8363cee747df146ffb9202b3d50ef0954901b97b
unknown <rubikonspace@ukr.net>
2023-10-25 00:42:41 +0300

    3

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -2,6 +2,8 @@ import { getMovieCredits } from 'helpers/API';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import defaultPic from 'download.jpg';
+
 export default function Cast() {
   const [aboutCasts, setAboutCasts] = useState([]);
   const params = useParams();
@@ -34,12 +36,17 @@ export default function Cast() {
     <div className="actor-card">
       {aboutCasts.length !== 0 &&
         aboutCasts.map(({ id, profile_path, name, character }) => {
+          const imageUrl = profile_path
+            ? `https://image.tmdb.org/t/p/w200${profile_path}`
+            : defaultPic;
+
           return (
             <div key={id}>
               <img
-                src={`https://image.tmdb.org/t/p/w200${profile_path}`}
+                src={imageUrl}
                 alt={name}
                 className="actor-image"
+                style={{ width: '300px' }}
               />
               <div className="actor-details">
                 <h3 className="actor-name">{name}</h3>
