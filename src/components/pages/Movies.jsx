a872426f182f31ea811f755d8dce1a604d50e062
unknown <rubikonspace@ukr.net>
2023-10-25 16:55:54 +0300

    search

--- src/components/pages/Movies.jsx
+++ src/components/pages/Movies.jsx
@@ -1,12 +1,30 @@
+import { useState } from 'react';
+
 export default function Movies() {
+  const [searchValue, setSearchValue] = useState('');
+
+  console.dir(searchValue);
+
   const hendleSubmitForm = evt => {
     evt.preventDefault();
+
+    setSearchValue('');
+  };
+
+  const handleChange = evt => {
+    const searchQueryValue = evt.target.value;
+    setSearchValue(searchQueryValue);
   };
 
   return (
     <>
       <form onSubmit={hendleSubmitForm}>
-        <input type="text" />
+        <input
+          type="text"
+          value={searchValue}
+          placeholder="Введіть пошуковий запит"
+          onChange={handleChange}
+        />
         <button type="submit">Search</button>
       </form>
     </>
