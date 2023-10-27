96c8332980fc2af70b4f7bcf6d7ceb616cbbe868
unknown <rubikonspace@ukr.net>
2023-10-28 02:18:38 +0300

    3

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -21,7 +21,7 @@ const Person = () => {
     };
 
     fetchPersonInfo();
-  }, [idPerson, details]);
+  }, [idPerson]);
   const {
     id,
     biography,
@@ -40,14 +40,14 @@ const Person = () => {
       <h2>{name}</h2>
       <img src={`https://image.tmdb.org/t/p/w200${profile_path}`} alt={name} />
       <h3>Biography</h3>
-      <p>{biography}</p>
+      <p>{biography ? biography : '-'}</p>
       <h3>Personal Information</h3>
       <h4>Also Known As: </h4>
-      <ul>
+      {/* <ul>
         {also_known_as.map(known => {
           return <li>{known}</li>;
         })}
-      </ul>
+      </ul> */}
       <ul>
         <li>
           <strong>Birthday:</strong> {birthday}
@@ -62,7 +62,7 @@ const Person = () => {
           <strong>Known for:</strong> {known_for_department}
         </li>
         <li>
-          <strong>Deathday:</strong> {deathday}
+          <strong>Deathday:</strong> {deathday ?? '-'}
         </li>
       </ul>
     </div>
