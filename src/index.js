7e919e912040bd2b461f19c8e7df0b814bfb118a
unknown <rubikonspace@ukr.net>
2023-10-28 16:12:17 +0300

    3

--- src/index.js
+++ src/index.js
@@ -7,10 +7,9 @@ import { GlobalStyle } from 'styles/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <App />
       <GlobalStyle />
     </BrowserRouter>
   </React.StrictMode>
 );
-// basename = '/goit-react-hw-05-movies';
