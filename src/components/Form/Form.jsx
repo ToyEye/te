804c70ffcbfe3c802b2693abdfc5d830e68eca87
unknown <rubikonspace@ukr.net>
2023-10-28 23:23:09 +0300

    3

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,13 +4,15 @@ const FormSearching = ({ querySearchMovies, defaultValue }) => {
     const inputValue = evt.target.elements.queryValueSearch.value.toLowerCase();
 
     querySearchMovies(inputValue);
+    evt.target.elements.queryValueSearch.value = '';
   };
 
   return (
     <form onSubmit={handleSubmitForm}>
       <input
         type="text"
-        // заюзав defaultValue замість value, тому що для нього тре onChange, а value я передаю із пропсів.
+        // заюзав defaultValue замість value, тому що для value тре onChange, а value я передаю із пропсів.
+        // defaultValue зарезерв. змінна
         defaultValue={defaultValue}
         placeholder="Введіть пошуковий запит"
         name="queryValueSearch"
